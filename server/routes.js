8422bd8e4da7b7ffebc4a0c13f6e4370eafcf19d
Jac <lu7766lu7766@gmail.com>
2017-10-05 00:16:34 +0800

    main移除git 新增orm: bookshelf, query builder: knex(含migrate & seeder 功能)

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,30 @@
 'use strict'
 
 import { Router } from 'express'
-import knex from 'knex' // query builder
+// import knex from 'knex' // query builder
+require('dotenv').config()
+var knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    host : process.env.DB_HOST || '127.0.0.1',
+    user : process.env.DB_USER || 'admin',
+    password : process.env.DB_PWD || '',
+    port : process.env.DB_PORT || '3306',
+    database : process.env.DB_NAME || 'myapp_test'
+  },
+  migrations: {
+    tableName: 'migrations'
+  }
+});
+var bookshelf = require('bookshelf')(knex);
+var User = bookshelf.Model.extend({
+  tableName: 'users'
+});
+// console.log('host', process.env.DB_HOST || '127.0.0.1')
+// console.log('user', process.env.DB_USER || 'admin')
+// console.log('password', process.env.DB_PWD || '')
+// console.log('port', process.env.DB_PORT || '3306')
+// console.log('database', process.env.DB_NAME || 'myapp_test')
 
 const router = Router()
 
@@ -25,8 +48,15 @@ export default () => {
   // })
 
   // Get all users.
-  router.get('/users', (req, res, next) => {
-    res.body = users
+  router.get('/users', async (req, res, next) => {
+    var user = await User.fetchAll()
+    // .then(function(user) {
+    //   console.log(user.toJSON());
+    // }).catch(function(err) {
+    //   console.error(err);
+    // });
+    // console.log(knex.select('*').from('users').toSQL())
+    res.body = user.toJSON()
     // res.status = 0
     next()
   })
